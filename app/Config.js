df1f6004e7ac58ff1a2dab17c5d0480bdeeca44b
omkar <omkar@saiashirwad.com>
2025-04-27 11:19:52 +0530

    config added

---
+++ app/Config.js
@@ -0,0 +1,14 @@
+const Config = {
+  PROJECT_URL: process.env.PROJECT_URL ,
+  PROJECT_SAVE_URL: process.env.PROJECT_SAVE_URL ,
+  AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME ,
+  AWS_S3_REGION: process.env.AWS_S3_REGION ,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY:
+    process.env.AWS_SECRET_ACCESS_KEY
+   ,
+  API_KEY:
+    process.env.API_KEY ,
+};
+
+export default Config;
